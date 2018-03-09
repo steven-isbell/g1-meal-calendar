f11ed8ea1bec804215819d16109d0a2dd8877eb8
Steven Isbell <steven.isbell18@gmail.com>
2018-03-09 15:05:07 -0600

    ...

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.delete("/api/event/:id", deleteEvent);
 app.patch("/api/event/:id", updateEvent);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/../build/index.html"));
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 app.listen(port, () => {
